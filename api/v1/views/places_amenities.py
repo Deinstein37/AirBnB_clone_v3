5ccdd93d9fd5f10841748784c5f3212c60f626ae
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-11-21 10:40:52 +0100

    Update places_amenities.py

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,92 +1,66 @@
 #!/usr/bin/python3
-"""
-Review view for API.
-
-"""
-
-from flask import abort, request, jsonify
-
+"""Module for Place related endpoints"""
 from api.v1.views import app_views
+from api.v1.views import *
+from flask import jsonify, make_response, abort, request
 from models import storage
-from models.review import Review
+
+model = "Amenity"
+parent_model = "Place"
 
 
-@app_views.route(
-    '/places/<place_id>/reviews',
-    methods=['GET'],
-    strict_slashes=False)
-def get_review_by_place(place_id):
-    """Returns JSON reviews of given place"""
-    place = storage.get('Place', place_id)
-    if place:
-        reviews = []
-        for review in place.reviews:
-            reviews.append(review.to_dict())
-        return (jsonify(reviews), 200)
-    abort(404)
+@app_views.route("/places/<place_id>/amenities", strict_slashes=False,
+                 methods=["GET"])
+def get_amenities(place_id):
+    """GET /place api route"""
+    return get_models(parent_model, place_id, "amenities")
 
 
-@app_views.route(
-    '/reviews/<review_id>',
-    methods=['GET'],
-    strict_slashes=False)
-def get_review(review_id):
-    """Returns JSON review with a given id"""
-    review = storage.get('Review', review_id)
-    if review:
-        return (jsonify(review.to_dict()), 200)
-    abort(404)
+@app_views.route("/places/<place_id>/amenities/<amenity_id>",
+                 strict_slashes=False, methods=["DELETE", "POST"])
+def process_amenity(place_id, amenity_id):
+    """Process request type"""
+    if request.method == "DELETE":
+        return delete_amenity(place_id, amenity_id)
+    else:
+        return post_amenity(place_id, amenity_id)
 
 
-@app_views.route(
-    '/reviews/<review_id>',
-    methods=['Delete'],
-    strict_slashes=False)
-def delete_review(review_id):
-    """Deletes review with a given id"""
-    review = storage.get('Review', review_id)
-    if review:
-        review.delete()
-        storage.save()
-        return (jsonify({}), 200)
-    abort(404)
+def delete_amenity(place_id, amenity_id):
+    """DELETE /amenity api route"""
+    place = storage.get(parent_model, place_id)
+    if not place:
+        return make_response(jsonify({"error": "Not found"}), 404)
+    amenity = storage.get(model, amenity_id)
+    if not amenity:
+        return make_response(jsonify({"error": "Not found"}), 404)
+
+    if amenity not in place.amenities:
+        return make_response(jsonify({"error": "Not found"}), 404)
+
+    # storage.delete(amenity)
+    # if getenv("HBNB_TYPE_STORAGE") != "db":
+    place.amenities.remove(amenity)
+    # else:
+    #    storage.delete(amenity)
+
+    storage.save()
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route(
-    '/reviews/<place_id>/reviews',
-    methods=['POST'],
-    strict_slashes=False)
-def post_review(place_id):
-    """Creates review for a given place"""
-    review_dict = request.get_json()
-    place = storage.get('Place', place_id)
-    if not review_dict:
-        return (jsonify({'error': 'Not a JSON'}), 400)
-    elif 'text' not in review_dict:
-        return (jsonify({'error': 'Missing text'}))
-    elif 'user_id' not in review_dict:
-        return (jsonify({'error': 'Missing user_id'}), 400)
-    elif storage.get('User', request.get_json()['user_id']) and place:
-        review = Review(**review_dict)
-        review.place_id = place_id
-        review.save()
-        return (jsonify(review.to_dict()), 201)
-    abort(404)
+def post_amenity(place_id, amenity_id):
+    """POST /amenities api route"""
+    place = storage.get(parent_model, place_id)
+    if not place:
+        return make_response(jsonify({"error": "Not found"}), 404)
+    amenity = storage.get(model, amenity_id)
+    if not amenity:
+        return make_response(jsonify({"error": "Not found"}), 404)
 
+    if amenity in place.amenities:
+        return make_response(jsonify(amenity.to_dict()), 200)
 
-@app_views.route(
-    '/reviews/<review_id>',
-    methods=['PUT'],
-    strict_slashes=False)
-def put_review(review_id):
-    """Updates review with a given id"""
-    ignore_keys = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
-    review_dict = request.get_json()
-    review = storage.get('Review', review_id)
-    if not review_dict:
-        return (jsonify({'error': 'Not a JSON'}), 400)
-    if review:
-        for key in review_dict.keys():
-            if key not in ignore_keys:
-                setattr(review, key, review_dict[key])
-    abort(404)
+    place.amenities.append(amenity)
+
+    place.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
